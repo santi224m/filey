f440eaf731882073150a49741df127ac62926ff4
santi224m <santiago.mondragon224@gmail.com>
2021-04-11 16:47:09 -0700

    add upload success-page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from   'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import SendFiles from './SendFiles';
+import SendFilesSuccess from './SendFilesSuccess';
 
 const App = () => {
     const [code, setCode] = useState(null);
@@ -13,6 +14,9 @@ const App = () => {
             <Switch>
                 <Route path="/send-files">
                     <SendFiles setCode={setCode} />
+                </Route>
+                <Route path="/upload-files-success">
+                    <SendFilesSuccess code={code} />
                 </Route>
                 <Route path="/" exact component={Home} /> 
             </Switch>
